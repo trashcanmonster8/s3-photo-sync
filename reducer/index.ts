8e4ada7bfcd936aeac8667e76c5d7e05d1f8083b
trashcanmonster8 <jacob.michael.mayer@gmail.com>
2021-10-27 14:26:30 -0500

    completed handler

--- reducer/index.ts
+++ reducer/index.ts
@@ -1,9 +1,23 @@
-import { Callback, Context, Handler, S3Event } from "aws-lambda";
+import { S3Client } from "@aws-sdk/client-s3";
+import { Handler, S3Event, S3EventRecord } from "aws-lambda";
+import { getImage } from "./helpers/getImage";
+import { parseResponse } from "./helpers/parseResponse";
+import { putImage } from "./helpers/putImage";
+import { reduceImage } from "./helpers/reduceImage";
 
-export const handler: Handler<S3Event, object> = async (
-  event: S3Event,
-  context: Context,
-  callback: Callback<object>
-): Promise<object> => {
-  return { event, context, callback };
+export const handler: Handler<S3Event, void> = async (
+  event: S3Event
+): Promise<void> => {
+  event.Records.forEach(async (record: S3EventRecord): Promise<void> => {
+    const bucketName = record.s3.bucket.name;
+    const reducedBucketName = bucketName + "-reduced";
+    const key = record.s3.object.key;
+    const client: S3Client = new S3Client({ region: record.awsRegion });
+    return getImage(client, bucketName, key)
+      .then(parseResponse)
+      .then(reduceImage)
+      .then((data: Buffer) => {
+        putImage(client, reducedBucketName, key, data);
+      });
+  });
 };
