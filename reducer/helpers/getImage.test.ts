89e3e4969892b0042c86d6173cb38605743e4b7c
trashcanmonster8 <jacob.michael.mayer@gmail.com>
2021-10-27 08:31:18 -0500

    get image stream from s3 bucket

---
+++ reducer/helpers/getImage.test.ts
@@ -0,0 +1,19 @@
+import { GetObjectOutput, S3Client } from "@aws-sdk/client-s3";
+import { strictEqual } from "assert";
+import { stub, SinonStub } from "sinon";
+import { Readable } from "stream";
+import { getImage } from "./getImage";
+
+describe('getImage', () => {
+    it('returns image buffer', async () => {
+        const client: S3Client = new S3Client({});
+        const bucketName = 'testName';
+        const objectKey = 'testKey';
+        const getObjectStub: SinonStub = stub(client, 'send');
+        const result: GetObjectOutput = {
+            Body: new Readable()
+        };
+        getObjectStub.resolves(result)
+        strictEqual(await getImage(client, bucketName, objectKey), result.Body);
+    });
+});
