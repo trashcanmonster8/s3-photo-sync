1bb280184aa4b8451e8c5890778e0a520d6d4148
trashcanmonster8 <jacob.michael.mayer@gmail.com>
2021-10-27 14:24:26 -0500

    added default value to reduce the image by

--- reducer/helpers/reduceImage.ts
+++ reducer/helpers/reduceImage.ts
@@ -2,7 +2,7 @@ import Jimp, { read } from "jimp";
 
 export const reduceImage = async (
   data: Buffer,
-  quality: number
+  quality = 25
 ): Promise<Buffer> => {
   const image: Jimp = await read(data);
   return image.quality(quality).getBufferAsync(Jimp.MIME_JPEG);
