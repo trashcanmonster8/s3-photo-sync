1e661d595f4aa783ba20d8b6d20f25bf50bf7be8
trashcanmonster8 <jacob.michael.mayer@gmail.com>
2021-10-27 09:03:09 -0500

    removed type errors called out in ide

--- reducer/index.ts
+++ reducer/index.ts
@@ -1,5 +1,5 @@
 import { Callback, Context, Handler, S3Event } from 'aws-lambda';
 
-export const handler: Handler<S3Event, object> = async (event: S3Event, context: Context, callback: Callback<void>): Promise<object> => {
+export const handler: Handler<S3Event, object> = async (event: S3Event, context: Context, callback: Callback<object>): Promise<object> => {
     return {event, context, callback};
 }
