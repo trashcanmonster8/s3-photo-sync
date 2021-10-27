e7762b80cda9b0c500c1d86bbcf1b88336c7e117
trashcanmonster8 <jacob.michael.mayer@gmail.com>
2021-10-27 09:51:42 -0500

    return aws response object

--- reducer/helpers/getImage.ts
+++ reducer/helpers/getImage.ts
@@ -4,19 +4,17 @@ import {
   GetObjectOutput,
   S3Client,
 } from "@aws-sdk/client-s3";
-import { Readable } from "stream";
 
 export const getImage = async (
   client: S3Client,
   bucket: string,
   key: string
-): Promise<Readable | ReadableStream | Blob | undefined> => {
+): Promise<GetObjectOutput> => {
   const params: GetObjectCommandInput = {
     Bucket: bucket,
     Key: key,
   };
-  const result: GetObjectOutput = await client.send(
+  return await client.send(
     new GetObjectCommand(params)
   );
-  return result.Body;
 };
