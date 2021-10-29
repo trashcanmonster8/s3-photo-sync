aa54a7c9c36e33b3180fa839c9cc1a928112c3d2
trashcanmonster8 <jacob.michael.mayer@gmail.com>
2021-10-29 14:51:08 -0500

    format fixes

--- reducer/index.ts
+++ reducer/index.ts
@@ -1,4 +1,8 @@
-import { GetObjectOutput, PutObjectCommandOutput, S3Client } from "@aws-sdk/client-s3";
+import {
+  GetObjectOutput,
+  PutObjectCommandOutput,
+  S3Client,
+} from "@aws-sdk/client-s3";
 import { Handler, S3Event, S3EventRecord } from "aws-lambda";
 import { getImage } from "./helpers/getImage";
 import { parseResponse } from "./helpers/parseResponse";
@@ -8,26 +12,28 @@ import { reduceImage } from "./helpers/reduceImage";
 export const handler: Handler<S3Event, void[]> = async (
   event: S3Event
 ): Promise<void[]> => {
-  return Promise.all(event.Records.map(async (record: S3EventRecord): Promise<void> => {
-    const bucketName = record.s3.bucket.name;
-    console.log(`Received event: ${record.eventName}`);
-    const reducedBucketName = bucketName + "-reduced";
-    const key = record.s3.object.key;
-    console.log(`S3 object to reduct: ${bucketName}/${key}`);
-    const client: S3Client = new S3Client({ region: record.awsRegion });
-    return getImage(client, bucketName, key)
-      .then((response: GetObjectOutput) => {
-        console.log(`Body is of type: ${typeof response.Body}`)
-        console.log(`Received ${key} etag: ${response.ETag}`)
-        return response;
-      })
-      .then(parseResponse)
-      .then(reduceImage)
-      .then((data: Buffer) => {
-        return putImage(client, reducedBucketName, key, data);
-      })
-      .then((response: PutObjectCommandOutput) => {
-        console.log(`Put reduced object etag: ${response.ETag}`)
-      });
-  }));
+  return Promise.all(
+    event.Records.map(async (record: S3EventRecord): Promise<void> => {
+      const bucketName = record.s3.bucket.name;
+      console.log(`Received event: ${record.eventName}`);
+      const reducedBucketName = bucketName + "-reduced";
+      const key = record.s3.object.key;
+      console.log(`S3 object to reduct: ${bucketName}/${key}`);
+      const client: S3Client = new S3Client({ region: record.awsRegion });
+      return getImage(client, bucketName, key)
+        .then((response: GetObjectOutput) => {
+          console.log(`Body is of type: ${typeof response.Body}`);
+          console.log(`Received ${key} etag: ${response.ETag}`);
+          return response;
+        })
+        .then(parseResponse)
+        .then(reduceImage)
+        .then((data: Buffer) => {
+          return putImage(client, reducedBucketName, key, data);
+        })
+        .then((response: PutObjectCommandOutput) => {
+          console.log(`Put reduced object etag: ${response.ETag}`);
+        });
+    })
+  );
 };
